c3d4a4cd8f00bbbf27dda2fa1c0cf415d60c761c
Nicole Kuhn <nskuhn03@gmail.com>
2018-01-16 16:16:32 -0800

    Add Exercise-wk2/ Lab participation

--- exercise-wk2/exercise1.R
+++ exercise-wk2/exercise1.R
@@ -1,4 +1,4 @@
-## Part 1: Debugging
+## Part 1: Debugging - Nicole Kuhn
 
 my.num <- 6
 initials <- "?. ?."
@@ -9,7 +9,8 @@ my.vector <- c(my.num, initials)
 vector.sum <- sum(my.vector)
 
 # Describe why this doesn't work: 
-# 
+# Error because "initials" variable contains characters, must be numeric values
+
 
 install.packages("stringr")
 
@@ -18,21 +19,30 @@ my.line <- "Hey, hey, this is the library"
 print(str_length(my.line))
 
 # Describe why this doesn't work: 
-# 
+# function "str_length" has not been defined, not a built-in function nor from stringr package
+
 
 said.the.famous <- paste(my.line, " - ", initial)
 
 # Describe why this doesn't work: 
-# 
+# the variable "initial" has not been defined
 
 
 ## Part 2 - Vector and function practice
 
 # Make a vector and use typeof to check what type R considers it to be
 
+people <- c("Sarah", "Amit", "Zhang")
+typeof(people)
+
+# type of function = character
+
+
 # Write a function `CompareLength` that takes in 2 vectors, and returns the sentence:
 # "The difference in lengths is N"
-
+CompareLength <- c(length1, length2){
+  
+}
 
 # Pass two vectors of different length to your `CompareLength` function
 
